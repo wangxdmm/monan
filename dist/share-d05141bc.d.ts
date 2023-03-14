7afdb09072f1f9a146a5fda574cc28629c900a20
wangxd <1121292341@qq.com>
2023-03-14 10:48:04 +0800

    chore: update ignore

--- dist/share-d05141bc.d.ts
+++
@@ -1,7 +0,0 @@
-type path = string | number;
-type pathArray = path[];
-declare function noop(..._args: any[]): void;
-declare function genAnyBackFunc<T = any>(t: T): () => T;
-declare function pathResolve(path: path | pathArray): string[];
-
-export { pathArray as a, pathResolve as b, genAnyBackFunc as g, noop as n, path as p };
